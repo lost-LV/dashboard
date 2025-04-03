e4482fe1857d8aad83018a37f842ba86df4a1107
lost-LV <lost@lvservices.online>
2025-04-03 03:43:57 +0200

    Add files via upload

---
+++ version.js
@@ -0,0 +1,6 @@
+// version.js - Stores the current version of the application
+window.appVersion = {
+    version: '1.0.2',
+    buildDate: new Date().toISOString(),
+    description: 'Initial release'
+};
